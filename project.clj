e52c619ba42652507e267f38cf550d7cb5a259fb
badjer <brian.dejong@gmail.com>
2011-11-24 14:44:03 -0800

    Updated the version number
    I'm not getting an updated version from clojars
    in my other project, so let's try inc'ing the version number

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject bootblack "0.0.1-SNAPSHOT"
+(defproject bootblack "0.0.2-SNAPSHOT"
   :description "Clojure web development with noir and bootstrap"
   :dependencies [[org.clojure/clojure "1.3.0"]
                  [noir "1.2.2-SNAPSHOT"]])
