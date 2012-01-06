2894ea58c389c56df7d1f2789b300e5d8e07c5a2
badjer <brian.dejong@gmail.com>
2012-01-06 15:19:49 -0800

    Added file-field

--- src/bootblack/core.clj
+++ src/bootblack/core.clj
@@ -16,7 +16,9 @@
               [:div {:class divclass} 
                [:label {:for formname} label]
                [:div {:class inputclass}
-                (inputgen {:class elemclass} formname value)
+                (if (nil? value)
+                  (inputgen {:class elemclass} formname)
+                  (inputgen {:class elemclass} formname value))
                 (vali/on-error nam error-item)]]))
 
 (defpartial text-field [nam label value]
@@ -25,8 +27,20 @@
 (defpartial password-field [nam label value]
             (input-field form/password-field nam label value))
 
+(defpartial file-field [nam label]
+            (input-field form/file-upload nam label nil))
+
 (defpartial form-to [legend submittxt url method & contents]
             [:form.form-stacked {:method method :action url}
+             [:fieldset
+              [:legend legend]
+              contents]
+             [:fieldset
+              [:div.actions
+               (form/submit-button {:class "btn primary"} submittxt)]]])
+
+(defpartial upload-form [legend submittxt url & contents]
+            [:form.form-stacked {:method :post :action url :enctype "multipart/form-data"}
              [:fieldset
               [:legend legend]
               contents]
